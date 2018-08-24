1f86d77ba63eac3a74f4eea53b7bf00403d4cf42
Rudmanmrrod <rudmanmrrod@gmail.com>
2018-08-24 15:59:04 -0400

    Agregada versión para producción

--- src/components/ListComics/ListComicsTemplate.vue
+++ src/components/ListComics/ListComicsTemplate.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="list">
-		<h1>Heroes List</h1>
+		<h1>Comics List</h1>
 		<v-jstree :data="asyncData" :async="getComics" show-checkbox multiple allow-batch whole-row @item-click="itemClick"></v-jstree>
 	</div>
   
